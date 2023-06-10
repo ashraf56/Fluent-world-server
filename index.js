f7651d5d32f965d7808bdebffa2a6e0a9414da48
ashraf56 <ashrafulfahim07@gmail.com>
2023-06-10 15:26:37 +0600

    alluser get method completed

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT|| 3000;
+require('dotenv').config()
 let cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+
 let jwt = require('jsonwebtoken');
 
 
 app.use(cors())
 app.use(express.json())
+
+let verifyToken= (req,res,next) =>{
+
+  let  authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SEC, (err, decoded) => 
+  {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access '  })
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5nj1o0g.mongodb.net/?retryWrites=true&w=majority`; 
 
@@ -38,16 +59,23 @@ async function run() {
 
       app.post('/alluser',async(req,res)=>{
         let user=req.body;
-        // let query={email: user.email}
-        // let existcoll= await alluserCollection.findOne(query);
-        // if (existcoll) {
-        //   return res.send({message:'already exist'})
-        // }
+        let query={email: user.email}
+        let Existsuser= await alluserCollection.findOne(query);
+        if (Existsuser) {
+          return res.send({message:'already exist'})
+        }
         const result = await alluserCollection.insertOne(user);
         res.send(result);
         
         })
 
+        app.get('/alluser', async(req,res)=>{
+
+          const result = await alluserCollection.find().toArray();
+          res.send(result);
+
+        })
+        
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
